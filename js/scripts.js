2a23c389414de0c9c7295fb887183905769fa00f
parkercatalano <student@epicodus.com>
2016-02-15 19:05:34 -0800

    add a separate 21 response q y

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 $(function() {
   var age = parseInt(prompt("How old are you?"));
 
-  if (age >= 21) {
+  if (age > 21) {
     $('#drinks').show();
+  }
+  else if (age === 21) {
+    alert("Don't go crazy");
+    $("#drinks").show();
   }
   else {
    $("#under-21").show();
